b5ba684bc8c7015d79d499e9844f8feaf6678af3
akashdeb <akashdeb87@gmail.com>
2024-07-13 14:38:56 +0530

    Added new Changes after holiday

--- demoApps/src/test/java/testNg/ClassA.java
+++ demoApps/src/test/java/testNg/ClassA.java
@@ -5,6 +5,10 @@ public class ClassA {
 	public static void main(String[] args) {
 
 		System.out.println("These are the new Changes");
+		
+		
+		
+		System.out.println("These are the changes after Bangkok");
 	}
 
 }
